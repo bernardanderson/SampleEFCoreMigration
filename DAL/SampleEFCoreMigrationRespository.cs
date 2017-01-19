de65222fcbb58cfa0df6a9651c31631cd487aca8
Bernard Anderson <bernard.anderson@gmail.com>
2017-01-19 11:09:09 -0600

    Added basic set up for Db Repository

---
+++ DAL/SampleEFCoreMigrationRespository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleEFCoreMigration.DAL
+{
+    public class SampleEFCoreMigrationRespository
+    {
+        public SampleEFCoreMigrationContext Context { get; set; }
+
+        public SampleEFCoreMigrationRespository(SampleEFCoreMigrationContext sentContext)
+        {
+            Context = sentContext;
+        }
+    }
+}
