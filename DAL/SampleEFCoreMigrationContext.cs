dc302034f992e4cd70094fa4738eaed338935786
Bernard Anderson <bernard.anderson@gmail.com>
2017-01-19 09:56:53 -0600

    Added the DbContext The commented code allows the overriding of the pluralization of the data tables

---
+++ DAL/SampleEFCoreMigrationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleEFCoreMigration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleEFCoreMigration.DAL
+{
+    public class SampleEFCoreMigrationContext : DbContext
+    {
+        public SampleEFCoreMigrationContext(DbContextOptions<SampleEFCoreMigrationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Chemical> Chemicals { get; set; }
+
+        // The commented code below allows you to override the default pluralization of the Table name 
+        //  Coders disagree of whether to pluralize table name or not... 
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+        }
+        */
+    }
+}
